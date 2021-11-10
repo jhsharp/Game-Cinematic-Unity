f6d3786899fa0855a7d48b7c072beae8e87675b2
jhsharp <73131537+jhsharp@users.noreply.github.com>
2021-11-10 16:51:18 -0600

    Alternate Trees in EnvNightMode Script

--- Sharp-UnityScene/Assets/Scripts/EnvNightMode.cs
+++ Sharp-UnityScene/Assets/Scripts/EnvNightMode.cs
@@ -7,29 +7,42 @@ public class EnvNightMode : MonoBehaviour
     public bool isParent = false;
     public bool nightMode = false;
     public GameObject dayObj;
+    public GameObject dayObj2;
     public GameObject nightObj;
+    public bool select = false;
 
     // Start is called before the first frame update
     void Start()
+    {
+        if (dayObj2 == null)
+        {
+            dayObj2 = dayObj;
+        }
+
+        dayObj.SetActive(false);
+        dayObj2.SetActive(false);
+        nightObj.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (!isParent)
         {
             if (transform.parent.gameObject.GetComponent<EnvNightMode>().nightMode)
             {
                 dayObj.SetActive(false);
+                dayObj2.SetActive(false);
                 nightObj.SetActive(true);
             }
             else
             {
-                dayObj.SetActive(true);
+                if (transform.parent.gameObject.GetComponent<EnvNightMode>().select) dayObj.SetActive(true);
+                else dayObj2.SetActive(true);
                 nightObj.SetActive(false);
             }
+            transform.parent.gameObject.GetComponent<EnvNightMode>().select = !transform.parent.gameObject.GetComponent<EnvNightMode>().select;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
